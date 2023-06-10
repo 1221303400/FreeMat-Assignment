62e2ebb1a3f6ea0a110a8d32b9b98f07185d785b
Mohd Farhan <122974978+1221303400@users.noreply.github.com>
2023-06-10 10:46:55 +0800

    Add files via upload

---
+++ Assignment Take 2/counterProbInit.m
@@ -0,0 +1,5 @@
+function counterProbInit()
+    global counter1Prob, global counter2Prob, global counterExProb;
+    counter1Prob = [0.20, 0.25, 0.35, 0.20];
+    counter2Prob = [0.15, 0.40, 0.20, 0.25];
+    counterExProb = [0.30, 0.35, 0.25, 0.10];
---
+++ Assignment Take 2/updateArray.m
@@ -0,0 +1,4 @@
+function updateArray()
+    global myArray;
+    myArray = [5,99,2,1,3];
+    end
