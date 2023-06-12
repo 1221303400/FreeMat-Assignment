8e91d7bc2f2dd8d3e8cb5ad42202ae125a4be8d3
Mohd Farhan <122974978+1221303400@users.noreply.github.com>
2023-06-12 12:40:16 +0800

    Delete Assignment directory

--- Assignment/serviceTimeInit.m
+++
@@ -1,9 +0,0 @@
-function serviceTimeInit()
-    % Service Time
-    global counterTimes
-    counter1Time = [5, 6, 7, 8, 9];
-    counter2Time = [5, 6, 7, 8, 9];
-    counterExTime = [2, 3, 4, 5];
-    counterTimes = {counter1Time, counter2Time, counterExTime};
-    
-    % output = counterTimes;
