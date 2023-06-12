5f05164f941e950c4a768975344939f24952a19a
Mohd Farhan <122974978+1221303400@users.noreply.github.com>
2023-06-12 08:45:38 +0800

    Delete Assignment directory

--- Assignment/myPause.m
+++
@@ -1,4 +0,0 @@
-function output = myPause() 
-    fprintf('\n');
-    output = getline('Press ENTER to continue...');
-    fprintf('\n');
--- Assignment/serviceTimeInit.m
+++
@@ -1,9 +0,0 @@
-function serviceTimeInit()
-    % Service Time
-    global counterTimes
-    counter1Time = [2, 3, 4, 5, 6];
-    counter2Time = [2, 3, 4, 5, 6];
-    counterExTime = [1, 2, 3, 4];
-    counterTimes = {counter1Time, counter2Time, counterExTime};
-    
-    % output = counterTimes;
